10878a14b8bf0f16749e3923bfd22007476580cd
Maciej Tokarczyk <maciek.tokarczyk1@gmail.com>
2023-02-08 11:56:57 +0100

    Add Search Component and Search Page

---
+++ src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-flex align-center">
+    <v-text-field
+      class="input__search"
+      label="Search movies..."
+      v-model="search"
+      @keydown.enter="store.fetchMovies()"
+      data-test="search"
+    ></v-text-field>
+    <v-btn
+      class="ml-2"
+      @click="store.fetchMovies()"
+      :loading="store.status === 'progress'"
+      :disabled="search === ''"
+      data-test="submit"
+      >Search</v-btn
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStore } from "@/composables/useStore";
+import { computed } from "vue";
+const store = useStore();
+const search = computed({
+  get: () => store.params.title,
+  set: (val) => store.setParams({ ...store.params, title: val }),
+});
+</script>
+
+<style>
+.input__search .v-input__details {
+  display: none;
+}
+</style>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <v-container>
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col :cols="8">
+          <Search />
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+      <v-row>
+        <v-col :cols="12" :sm="6" :md="8"></v-col>
+        <v-col :cols="12" :sm="6" :md="4"></v-col>
+      </v-row>
+    </v-container>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import Search from "@/components/Search.vue";
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
